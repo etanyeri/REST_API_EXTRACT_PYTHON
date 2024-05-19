7ecca5bf90daee150d375d7d988e09876ba878f7
etanyeri <94400650+etanyeri@users.noreply.github.com>
2024-05-18 22:13:09 -0700

    Create jira_extract.py

---
+++ jira_extract.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import json
+import logging
+
+sys.path.append("/user/local/airflow/dags/")
+sys.path.append(os.path.dirname(__file__))
+
+from jira.get_jira_data import pull_jira_data
+from airflow.models import variable
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+import utils
+import dag_utils
+
+
+if os.environ.get("CI"):
+  S3_BUCKET_ROOT = "TEST_S3_BUCKET"
+  S3_BRANCH_ALIAS = "MR"
+  AIRFLOW__WEBSERVER__BASE_URL = "URL"
+else:
+  
